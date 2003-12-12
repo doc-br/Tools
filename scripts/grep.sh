37876b7cbf02c646168ddd9e4ea7caf3a03d6d31
gamk <gamk>
2003-12-12 12:20:13 +0000

    Adaptacao do Script para checar um espaco depois do <para> e/ou um antes do </para> Diego Linke

--- scripts/grep.sh
+++ scripts/grep.sh
@@ -8,3 +8,6 @@ echo "-----> dois espacos separando palavras <-----"
 grep -nE "\w  (\w|&|%)" "${@}"
 echo "-----> um espaco depois de separador frasal <-----"
 grep -nE "(\.|\?|\!) (\w|$)" "${@}"
+echo "-----> um espaco depois do <para> ou antes do </para> <-----"
+grep -nE "(<para>\ |\ </para>)" "${@}"
+
